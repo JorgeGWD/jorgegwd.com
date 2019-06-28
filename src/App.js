0e1659a7b128c804b36e1d233c3ae51e0dbc6ee7
JorgeGWD <jorge.rodriguez.r@gmail.com>
2019-06-28 18:02:53 -0500

    Create Home Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 import './App.css';
 import './reset.css';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
 
       <Header />
+      <Home />
 
     </div>
   );
