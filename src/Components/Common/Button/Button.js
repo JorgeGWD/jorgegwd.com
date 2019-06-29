231504c47ef9bed8e1f1f4fb6b474e6fb19314fb
JorgeGWD <jorge.rodriguez.r@gmail.com>
2019-06-28 21:38:58 -0500

    Create Button component

---
+++ src/Components/Common/Button/Button.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Button.css';
+
+class Button extends Component {
+    render() {
+        return(
+            <button>
+                <p>Tell me about your project</p>
+            </button>
+        );
+    }
+}
+
+export default Button;
