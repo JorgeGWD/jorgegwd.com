2bd69895b1d3ab4242d91548858718ad3addf091
JorgeGWD <jorge.rodriguez.r@gmail.com>
2019-06-28 21:28:30 -0500

    Create Typing Component

---
+++ src/Components/Home/Typing/Typing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Typing.css';
+
+class Typing extends Component {
+    render() {
+        return(
+            <div className="typing">
+                <h1>We can create greats things together.</h1>
+            </div>
+        );
+    }
+}
+
+export default Typing;
