b7b0efaf53c6f4d5f19fcd989cb5ca6ef6d2cd8c
jorgegwd <jorge.rodriguez.r@gmail.com>
2020-07-31 23:39:29 -0500

    About grid

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -7,7 +7,7 @@ const About = () => (
         <div className="about content">
             <section>
                 <h1 className="section-title">About</h1>
-                <div className="section content">
+                <div className="section-content">
                     <div className="profile-description">
                         <p>Hi I'm <span>Jorge Rodríguez</span>, I have seven years working as a <span>FrontEnd Developer</span> and <span>Web Designer</span>, exercising with great enthusiasm my work with different companies and in a freelance manner.</p>
                         <p>I have had to develop some projects from scratch and others already started, as well as redesign some already finished, always looking to <span>implement new tools and technologies</span> that allow me to obtain <span>the best result</span>.</p>
