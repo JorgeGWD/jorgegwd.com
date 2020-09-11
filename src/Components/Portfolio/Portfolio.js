948ecd85d694944ed35c9392de511ffea20bfeb8
jorgegwd <jorge.rodriguez.r@gmail.com>
2020-09-11 17:43:34 -0500

    Set portfolio data

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Portfolio.css'
 
 const Portfolio = () => {
+
+    const [ data ] = useState([
+        {
+            image: 'Image URL',
+            title: 'Title',
+            description: 'Description'
+        }
+    ])
+
     return (
         <div className="container">
             <div className="portfolio content">
                 <section>
                     <h1>Portfolio</h1>
+                    <div className="portfolio-info box">
+                        {
+                            data.map((data, key) =>
+                            <div className="porfolio-data" key={key}>
+                                <p>{data.image}</p>
+                                <h3>{data.title}</h3>
+                                <p>{data.description}</p>
+                            </div>
+                            )
+                        }
+                    </div>
                 </section>
             </div>
         </div>
