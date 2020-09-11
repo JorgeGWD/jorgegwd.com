c1abe7b8581a9b40d6c5aad7313ec7909a057a15
jorgegwd <jorge.rodriguez.r@gmail.com>
2020-09-10 21:07:45 -0500

    Add Experience

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Skills.css"
 
 const Skills = () => {
+
+    const [ data ] = useState(
+        [
+            {
+                title: "Frontend Developer",
+                company: "Laika",
+                date: "December 2019 - March 2020"
+            },
+            {
+                title: "Frontend Developer",
+                company: "Tigy",
+                date: "July 2019 - October 2019"
+            },
+            {
+                title: "Development Lead / Frontend",
+                company: "SoftArs",
+                date: "April 2018 - April 2019"
+            },
+            {
+                title: "Development Lead / Frontend",
+                company: "Minami Digital",
+                date: "October 2017 - April 2018"
+            },
+            {
+                title: "Frontend Developer",
+                company: "Lear Express",
+                date: "October 2014 - August 2015"
+            },
+            {
+                title: "Web Developer and Graphic Designer",
+                company: "Fundación CENAMEC",
+                date: "July 2013 - September 2014"
+            },
+            {
+                title: "Web Developer",
+                company: "Tecnología Venova, C.A.",
+                date: "August 2010 - September 2011"
+            }
+    ])
     return (
         <div className="container">
             <div className="skills content">
@@ -9,6 +48,17 @@ const Skills = () => {
                     <section>
                         <div className="experience-section">
                             <h1>Experience</h1>
+                            <div className="experience">
+                                {
+                                    data.map((data, key) =>
+                                        <div id={data.id} className="experience-data box" key={key}>
+                                            <h2>{data.title}</h2>
+                                            <span>{data.company}</span>
+                                            <p>{data.date}</p>
+                                        </div>
+                                    )
+                                }
+                            </div>
                         </div>
                         <div className="skills-section">
                             <h1>Skills</h1>
