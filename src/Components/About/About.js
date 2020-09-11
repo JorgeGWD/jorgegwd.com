ab5c5a996e3ec89b5ed8799571fb204cd82d3ee9
jorgegwd <jorge.rodriguez.r@gmail.com>
2020-09-10 20:03:04 -0500

    About add data with hooks

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProfilePic from "../../assets/images/profile-pic.jpg"
 import './About.css'
 
-const About = () => (
-    <div className="container">
-        <div className="about content">
-            <section>
-                <h1 className="section-title">About</h1>
-                <div className="section-content">
-                    <div className="profile-description">
-                        <p>Hi I'm <span>Jorge Rodríguez</span>, I have seven years working as a <span>FrontEnd Developer</span> and <span>Web Designer</span>, exercising with great enthusiasm my work with different companies and in a freelance manner.</p>
-                        <p>I have had to develop some projects from scratch and others already started, as well as redesign some already finished, always looking to <span>implement new tools and technologies</span> that allow me to obtain <span>the best result</span>.</p>
-                        <p>To make this possible, I also dedicate myself to researching and learning about everything related to <span>new web development technologies</span>. This learning process has been part of my life for quite some time now and I hope it will be for much more.</p>
+const About = () => {
+
+    const [ data ] = useState(
+            [
+                {
+                text :
+                "<p>Hi I'm <span>Jorge Rodríguez</span>, I have seven years working as a <span>FrontEnd Developer</span> and <span>Web Designer</span>, exercising with great enthusiasm my work with different companies and in a freelance manner.</p>" +
+                "<p>I have had to develop some projects from scratch and others already started, as well as redesign some already finished, always looking to <span>implement new tools and technologies</span> that allow me to obtain <span>the best result</span>.</p>" +
+                "<p>To make this possible, I also dedicate myself to researching and learning about everything related to <span>new web development technologies</span>. This learning process has been part of my life for quite some time now and I hope it will be for much more.</p>"
+            }
+        ]
+    )
+
+    return (
+        <div className="container">
+            <div className="about content">
+                <section>
+                    <h1 className="section-title">About</h1>
+                    <div className="section-content">
+                        <div className="profile-description">
+                            {
+                                data.map((data, key) => 
+                                    <div className="data-text" key={key} dangerouslySetInnerHTML={{
+                                        __html: data.text
+                                    }}>
+                                    </div>
+                                )
+                            }
+                        </div>
+                        <div className="profile-pic">
+                            <img src={ProfilePic} alt="JorgeGWD"/>
+                        </div>
                     </div>
-                    <div className="profile-pic">
-                        <img src={ProfilePic} alt="JorgeGWD"/>
-                    </div>
-                </div>
-            </section>
+                </section>
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default About
