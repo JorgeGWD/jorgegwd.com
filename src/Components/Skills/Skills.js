237ccf26c90aff2599181d9aa4b44aa3b92436c5
jorgegwd <jorge.rodriguez.r@gmail.com>
2020-09-10 20:28:40 -0500

    Add Skills

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -7,7 +7,18 @@ const Skills = () => {
             <div className="skills content">
                 <div className="skills-box">
                     <section>
-                        <h1>Skills</h1>
+                        <div className="experience-section">
+                            <h1>Experience</h1>
+                        </div>
+                        <div className="skills-section">
+                            <h1>Skills</h1>
+                            <h2>Tools</h2>
+                            <p>Visual Studio Code, Sublime Text, Git (Github, Bitbucket, Gitlab), Jira, Trello, Slack.</p>
+                            <h2>Languajes</h2>
+                            <p>Javascript, PHP, HTML/5, CSS/3, Bash.</p>
+                            <h2>Frameworks And Libraries</h2>
+                            <p>React, Redux, React Native, SASS, LESS, MaterialUI for React, Bootstrap, Materialize CSS, JQuery.</p>
+                        </div>
                     </section>
                 </div>
             </div>
