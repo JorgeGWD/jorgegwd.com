b03479b1a95bfa8b5c6a74dc7e558d631d637d8c
jorgegwd <jorge.rodriguez.r@gmail.com>
2020-09-11 23:20:05 -0500

    More portfolio data

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -5,11 +5,11 @@ const Portfolio = () => {
 
     const [ data ] = useState([
         {
-            image: require('../../assets/images/portfolio-tigy-web.jpg'),
-            title: 'Tigy web',
-            description: 'Web app / Social network.',
-            tech: 'ReacJS, HTML5, React Router Dom, Sass, Javascript.',
-            url: 'https://tigyweb.tigyapp.com/'
+            image: require('../../assets/images/portfolio-luvania-web.jpg'),
+            title: 'Luvania',
+            description: 'Landing page.',
+            tech: 'NextJs, ReacJS, HTML5, Sass, Javascript.',
+            url: 'https://tratamientos-medicos-co-pmqajvfj2.vercel.app/'
         },
         {
             image: require('../../assets/images/portfolio-softars-web.jpg'),
@@ -19,18 +19,53 @@ const Portfolio = () => {
             url: 'http://softars.com/'
         },
         {
-            image: require('../../assets/images/portfolio-luvania-web.jpg'),
-            title: 'Luvania',
-            description: 'Landing page.',
-            tech: 'NextJs, ReacJS, HTML5, Sass, Javascript.',
-            url: 'https://tratamientos-medicos-co-pmqajvfj2.vercel.app/'
-        },
-        {
             image: require('../../assets/images/portfolio-laika-app.jpg'),
             title: 'Laika app',
             description: 'Mobile app.',
             tech: 'Reac Native, Flex, React Router Dom, Javascript.',
             url: 'https://laika.com.co/city/bog/dog'
+        },
+        {
+            image: require('../../assets/images/portfolio-tigy-web.jpg'),
+            title: 'Tigy web',
+            description: 'Web app / Social network.',
+            tech: 'ReacJS, HTML5, React Router Dom, Sass, Javascript.',
+            url: 'https://tigyweb.tigyapp.com/'
+        },
+        {
+            image: require('../../assets/images/portfolio-lorusso-web.jpg'),
+            title: 'T.S. Lorusso',
+            description: 'Company website.',
+            tech: 'HTML5, Css3, Javascript.',
+            url: 'http://www.lorussonet.com/'
+        },
+        {
+            image: require('../../assets/images/portfolio-equifax-web.jpg'),
+            title: 'Equifax Ignite™',
+            description: 'Demo page.',
+            tech: 'HTML5, Css3, Javascript.',
+            //url: 'https://laika.com.co/city/bog/dog'
+        },
+        {
+            image: require('../../assets/images/portfolio-konvergy-web.jpg'),
+            title: 'Konvergy web',
+            description: 'Demo page.',
+            tech: 'Web design, HTML5, Css3, Javascript.',
+            //url: 'https://laika.com.co/city/bog/dog'
+        },
+        {
+            image: require('../../assets/images/portfolio-hollywood-hel-web.jpg'),
+            title: 'Hollywood heladeria web',
+            description: 'Demo page.',
+            tech: 'Web design, HTML5, Css3, Javascript.',
+            //url: 'https://laika.com.co/city/bog/dog'
+        },
+        {
+            image: require('../../assets/images/portfolio-mealtohome-web.jpg'),
+            title: 'Delivery web',
+            description: 'Demo page.',
+            tech: 'Web design, HTML5, Css3, Javascript.',
+            //url: 'https://laika.com.co/city/bog/dog'
         }
     ])
 
