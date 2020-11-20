2ed0f44a38518f72e714fb21d8c6bd215075cf1f
JorgeGWD <jorge.rodriguez.r@gmail.com>
2020-11-20 16:36:37 -0500

    Ajust skills sectios styles

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -22,7 +22,7 @@ const Skills = () => {
                 company: "SoftArs",
                 date: "April 2018 - April 2019",
                 functions: {
-                    description: "<p>Study and implementation of new web technologies, team management and workflow development.</p>",
+                    description: "Study and implementation of new web technologies, team management and workflow development.",
                     details: ""
                 }
             },
@@ -31,7 +31,7 @@ const Skills = () => {
                 company: "Minami Digital",
                 date: "October 2017 - April 2018",
                 functions: {
-                    description: "<p>Develop layout web according to design approved by the client</p>",
+                    description: "Develop layout web according to design approved by the client",
                     details: "<li>Use of CMS (WordPress)</li>"+
                     "<li>Use React JS to build a dashboard for Aera</li>"
                 }
@@ -41,7 +41,7 @@ const Skills = () => {
                 company: "Lear Express",
                 date: "October 2014 - August 2015",
                 functions: {
-                    description: "<p>Redesign of the website and web api, achieving a more friendly design and intuitive for service users of the company. In addition to using analytics tools to optimize load times and improve SEO management</p>",
+                    description: "Redesign of the website and web api, achieving a more friendly design and intuitive for service users of the company. In addition to using analytics tools to optimize load times and improve SEO management",
                     details: "<li>Use of CMS (WordPress) for better handling and modification of content</li>"+
                     "<li>Use and modification of plugins</li>"+
                     "<li> SEO Analysis and Optimization</li>"+
@@ -53,7 +53,7 @@ const Skills = () => {
                 company: "Fundación CENAMEC",
                 date: "July 2013 - September 2014",
                 functions: {
-                    description: "<p>Development of a portal for the dissemination of news about the works, events and achievements of the foundation. Implementation of a distribution system of digital material for national teacher training</p>",
+                    description: "Development of a portal for the dissemination of news about the works, events and achievements of the foundation. Implementation of a distribution system of digital material for national teacher training",
                     details: "<li>Creation of digital platforms using a CMS (Joomla)</li>"+
                     "<li>Use and modification of plugins for managing news and events.</li>"+
                     "<li>Using SEO to position both portals.</li>"+
@@ -66,7 +66,7 @@ const Skills = () => {
                 company: "Tecnología Venova, C.A.",
                 date: "August 2010 - September 2011",
                 functions: {
-                    description: "<p>Design and development of the company website and creation of a catalog for offer the products and services thereof</p>",
+                    description: "Design and development of the company website and creation of a catalog for offer the products and services thereof",
                     details: "<li>Layout in HTML and layout in CSS and JavaScript</li>"+
                     "<li>Implementation of a CMS (Joomla) to create a catalog of products and services.</li>"+
                     "<li>Use and modification of plugins.</li>"
@@ -90,7 +90,7 @@ const Skills = () => {
             },
             {
                 title: "Other skills",
-                skills: "Wordpress, Design UI/UX, Responsive design."
+                skills: "Wordpress, Design UI/UX, Responsive design, API manipulation."
             }
     ])
 
@@ -104,15 +104,24 @@ const Skills = () => {
                             <div className="experience">
                                 {
                                     experience.map((experience, key) =>
-                                        <div id={experience.id} className="experience-data box" key={key}>
-                                            <div className="experience-data-info title">
-                                                <FontAwesomeIcon icon={faDesktop} /><h2>{experience.title}</h2>
+                                        <div className="experience-data" key={key}>
+                                            <div className="box">
+                                                <div className="experience-data-info title">
+                                                    <FontAwesomeIcon icon={faDesktop} /><h2>{experience.title}</h2>
+                                                </div>
+                                                <div className="experience-data-info place">
+                                                    <FontAwesomeIcon icon={faBuilding} /><span>{experience.company}</span>
+                                                </div>
+                                                <div className="experience-data-info date">
+                                                    <FontAwesomeIcon icon={faCalendarAlt} /><p>{experience.date}</p>
+                                                </div>
                                             </div>
-                                            <div className="experience-data-info place">
-                                                <FontAwesomeIcon icon={faBuilding} /><span>{experience.company}</span>
-                                            </div>
-                                            <div className="experience-data-info date">
-                                                <FontAwesomeIcon icon={faCalendarAlt} /><p>{experience.date}</p>
+                                            <div className="list">
+                                                    <p>{experience.functions.description}</p>
+                                                <ul dangerouslySetInnerHTML={{
+                                                    __html: experience.functions.details
+                                                }}>
+                                                </ul>
                                             </div>
                                         </div>
                                     )
