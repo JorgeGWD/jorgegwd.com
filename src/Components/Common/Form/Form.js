b16c3e933c96f411cc1ff35b2ae3cc9932766c91
JorgeGWD <jorge.rodriguez.r@gmail.com>
2020-11-24 15:00:50 -0500

    Contact form validation, submitting message

--- src/Components/Common/Form/Form.js
+++ src/Components/Common/Form/Form.js
@@ -1,10 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Button from '../Button/Button'
 import './Form.css'
 
 const Form = () => {
 
-    const [data, setData] = useState()
+    const [ data, setData ] = useState({
+        name: '',
+        // country: '',
+        email: '',
+        message: ''
+    })
+
+    const [ errors, setErrors ] = useState({})
+
+    const [ isSubmitting, setIsSubmitting ] = useState(false)
 
     const handleChange = (e) => {
         // console.log(e.target.value)
@@ -14,20 +23,81 @@ const Form = () => {
         })
     }
 
+    const validateInfo = (data) => {
+        let errors = {}
+    
+        if(!data.name.trim()) {
+            errors.name = "This field is required"
+        }
+    
+        /*if(!data.country) {
+            errors.country = "This field is required"
+        }*/
+
+        if(!data.email) {
+            errors.email = "This field is required"
+        } else if(!/\S+@\S+\.\S+/.test(data.email)) {
+            errors.email = "Invalid email"
+        }
+
+        if(!data.message) {
+            errors.message = "This field is required"
+        }
+        
+        return errors;
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(data);
+        
+        if(!data.name || !data.email || !data.message) {
+            setErrors(validateInfo(data))
+
+            // console.log('Error')
+            
+        } else {
+            setErrors(validateInfo(data))
+
+            setIsSubmitting(true)
+            
+            // console.log(data)
+
+            e.target.reset()
+
+        }
     }
+
+    useEffect(() => {
+        if(Object.keys(errors).length === 0 && isSubmitting) {
+            console.log(data)
+
+            setData(prevState => {
+                return {...prevState, name: '', email: '', message: ''}
+            })
+        }
+    }, [errors])
 
     return (
         <div className="contact-form">
-            <h1>Say hello!</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="text" name="name" placeholder="Enter you name" onChange={handleChange} />
-                <input type="email" name="email" placeholder="Enter your email address" onChange={handleChange} />
-                <textarea name="message" rows="6" placeholder="Your message here..." onChange={handleChange} />
-                <Button className={"primary-btn"} type={"submit"} text="Submit" />
-            </form>
+            <div className={isSubmitting ? "form hide" : "form active" } >
+                <h2>Say hello!</h2>
+                <form onSubmit={handleSubmit} noValidate autoComplete="off" >
+                    {errors.name && <p className="error-text">{errors.name}</p>}
+                    <input type="text" name="name" placeholder="Enter your name" onChange={handleChange} required />
+                    {errors.email && <p className="error-text">{errors.email}</p>}
+                    <input type="email" name="email" placeholder="Enter your email address" onChange={handleChange} />
+                    {errors.message && <p className="error-text">{errors.message}</p>}
+                    <textarea name="message" rows="6" placeholder="Your message here..." onChange={handleChange} />
+                    <Button className={"primary-btn"} type={"submit"} text="Submit" />
+                </form>
+            </div>
+            <div className={isSubmitting ? "confirmation active" : "confirmation"}>
+                <div className="confirmation-content">
+                    <h2>Great!</h2>
+                    <p>Thank you for contacting me <br/>I will be in touch with you soon</p>
+                    <Button className={"primary-btn"} text="Ok" />
+                </div>
+            </div>
         </div>
     )
 }
