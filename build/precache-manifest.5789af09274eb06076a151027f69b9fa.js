9471032caa4fa3c7a81fe7b147bf721b33c1aaf0
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-07-24 11:07:25 -0500

    New deploy

---
+++ build/precache-manifest.5789af09274eb06076a151027f69b9fa.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ccbf76cec7d46d91cd195328945ee9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72fc04cec9c242c4d1ac",
+    "url": "/static/css/main.a60e5b8a.chunk.css"
+  },
+  {
+    "revision": "68e7a5849eaf9c90bdd5",
+    "url": "/static/js/2.587d9e6b.chunk.js"
+  },
+  {
+    "revision": "72fc04cec9c242c4d1ac",
+    "url": "/static/js/main.6e26bca8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "414c2fcda54238526183d2e0649ea592",
+    "url": "/static/media/bruhspainted-bg.414c2fcd.svg"
+  },
+  {
+    "revision": "5d0a54828c7d06b5fa75496c9e119e0b",
+    "url": "/static/media/bruhspainted-bottom.5d0a5482.svg"
+  },
+  {
+    "revision": "a14e51e257c9ec1dd37a42a6f96eaff5",
+    "url": "/static/media/bruhspainted-top.a14e51e2.svg"
+  },
+  {
+    "revision": "6b9bd09fea548c2cd21d4d7aa74c53a1",
+    "url": "/static/media/experience-bg.6b9bd09f.jpg"
+  },
+  {
+    "revision": "dd29f64c71bb30a992a1c97cae759b72",
+    "url": "/static/media/logo.dd29f64c.svg"
+  },
+  {
+    "revision": "01bf211c59a8fd82cfd70b32fc48d961",
+    "url": "/static/media/portfolio-equifax-web.01bf211c.jpg"
+  },
+  {
+    "revision": "c45efa3f374292e482fd497b3543e4cb",
+    "url": "/static/media/portfolio-hollywood-hel-web.c45efa3f.jpg"
+  },
+  {
+    "revision": "2862c8ab2a2984ae0ef55cc25044e26a",
+    "url": "/static/media/portfolio-kiero-web.2862c8ab.jpg"
+  },
+  {
+    "revision": "a7f7241225f28f8d3cf865d845225f5a",
+    "url": "/static/media/portfolio-konvergy-web.a7f72412.jpg"
+  },
+  {
+    "revision": "2f914c69f34010b54a90e6ddc20ca85e",
+    "url": "/static/media/portfolio-laika-app.2f914c69.jpg"
+  },
+  {
+    "revision": "60a2c17e6a14aac66527f31a3eec7bb9",
+    "url": "/static/media/portfolio-lorusso-web.60a2c17e.jpg"
+  },
+  {
+    "revision": "452a9473ce74353d5f6d272e5718b840",
+    "url": "/static/media/portfolio-luvania-web.452a9473.jpg"
+  },
+  {
+    "revision": "0c40a5ee734d920514ce81406f00e14d",
+    "url": "/static/media/portfolio-mealtohome-web.0c40a5ee.jpg"
+  },
+  {
+    "revision": "f6a1b607f76dffa85f7ea4f97adbf991",
+    "url": "/static/media/portfolio-softars-web.f6a1b607.jpg"
+  },
+  {
+    "revision": "0038dfe9bf32d92950c109097a2fa6c1",
+    "url": "/static/media/portfolio-tigy-web.0038dfe9.jpg"
+  },
+  {
+    "revision": "b0d1a90fe0d2c93f876f515b8d4b5a55",
+    "url": "/static/media/profile-pic.b0d1a90f.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/JorgeGWD/jorgegwd-web/precache-manifest.e92f0c67be8e7fdaeeee6c3c6a1783c2.js"
+  "/precache-manifest.5789af09274eb06076a151027f69b9fa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/JorgeGWD/jorgegwd-web/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
