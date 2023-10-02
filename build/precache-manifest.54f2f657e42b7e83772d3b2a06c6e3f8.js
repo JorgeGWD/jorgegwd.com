2bf59602c7276c28377cb66ac8738800762886a9
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-10-02 17:25:38 -0500

    2023/10/02 deploy

---
+++ build/precache-manifest.54f2f657e42b7e83772d3b2a06c6e3f8.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "845f8cfc46672c05dc2b",
+    "url": "/static/css/main.5f7d3c29.chunk.css"
+  },
+  {
+    "revision": "845f8cfc46672c05dc2b",
+    "url": "/static/js/main.d8bb6bc5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8e52a15c4d2b50b57bc4",
+    "url": "/static/js/2.26f731af.chunk.js"
+  },
+  {
+    "revision": "dd29f64c71bb30a992a1c97cae759b72",
+    "url": "/static/media/logo.dd29f64c.svg"
+  },
+  {
+    "revision": "17a0583308f0d9f0cedd134ed4ef29a4",
+    "url": "/static/media/profile-pic.17a05833.jpg"
+  },
+  {
+    "revision": "6b9bd09fea548c2cd21d4d7aa74c53a1",
+    "url": "/static/media/experience-bg.6b9bd09f.jpg"
+  },
+  {
+    "revision": "414c2fcda54238526183d2e0649ea592",
+    "url": "/static/media/bruhspainted-bg.414c2fcd.svg"
+  },
+  {
+    "revision": "a14e51e257c9ec1dd37a42a6f96eaff5",
+    "url": "/static/media/bruhspainted-top.a14e51e2.svg"
+  },
+  {
+    "revision": "5d0a54828c7d06b5fa75496c9e119e0b",
+    "url": "/static/media/bruhspainted-bottom.5d0a5482.svg"
+  },
+  {
+    "revision": "452a9473ce74353d5f6d272e5718b840",
+    "url": "/static/media/portfolio-luvania-web.452a9473.jpg"
+  },
+  {
+    "revision": "2862c8ab2a2984ae0ef55cc25044e26a",
+    "url": "/static/media/portfolio-kiero-web.2862c8ab.jpg"
+  },
+  {
+    "revision": "2f914c69f34010b54a90e6ddc20ca85e",
+    "url": "/static/media/portfolio-laika-app.2f914c69.jpg"
+  },
+  {
+    "revision": "0038dfe9bf32d92950c109097a2fa6c1",
+    "url": "/static/media/portfolio-tigy-web.0038dfe9.jpg"
+  },
+  {
+    "revision": "f6a1b607f76dffa85f7ea4f97adbf991",
+    "url": "/static/media/portfolio-softars-web.f6a1b607.jpg"
+  },
+  {
+    "revision": "60a2c17e6a14aac66527f31a3eec7bb9",
+    "url": "/static/media/portfolio-lorusso-web.60a2c17e.jpg"
+  },
+  {
+    "revision": "01bf211c59a8fd82cfd70b32fc48d961",
+    "url": "/static/media/portfolio-equifax-web.01bf211c.jpg"
+  },
+  {
+    "revision": "a7f7241225f28f8d3cf865d845225f5a",
+    "url": "/static/media/portfolio-konvergy-web.a7f72412.jpg"
+  },
+  {
+    "revision": "c45efa3f374292e482fd497b3543e4cb",
+    "url": "/static/media/portfolio-hollywood-hel-web.c45efa3f.jpg"
+  },
+  {
+    "revision": "0c40a5ee734d920514ce81406f00e14d",
+    "url": "/static/media/portfolio-mealtohome-web.0c40a5ee.jpg"
+  },
+  {
+    "revision": "0a2d2ca79a1f1d83e2b60507bb90248e",
+    "url": "/index.html"
+  }
+];
