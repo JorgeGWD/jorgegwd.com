045a39b7af3f95e66d558982e2f457fe5ca65c10
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-10-25 14:56:21 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.69102724c40302cd756963f982825be8.js"
+  "/precache-manifest.b4fcab8a59106d346a352ea2b450a14d.js"
 );
 
 workbox.clientsClaim();
