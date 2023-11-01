885b48c2593dfe69ff286c6380df77b8be325124
JorgeGWD <jorge.rodriguez.r@gmail.com>
2023-11-01 15:22:11 -0500

    Add typing and button animation

--- src/Components/Home/Typing/Typing.js
+++ src/Components/Home/Typing/Typing.js
@@ -1,75 +1,94 @@
-import React from 'react'
-import './Typing.css'
+import React, { useEffect, useState } from 'react';
+import './Typing.css';
 
-var TxtRotate = function(el, toRotate, period) {
+var TxtRotate = function (el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
     this.loopNum = 0;
     this.period = parseInt(period, 10) || 2000;
     this.txt = '';
-    this.tick();
+    this.tick = () => {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span className="wrap">' + this.txt + '</span>';
+
+        var that = this;
+        var delta = 300 - Math.random() * 100;
+
+        if (this.isDeleting) {
+        delta /= 2;
+        }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function () {
+        that.tick();
+        }, delta);
+    };
     this.isDeleting = false;
- };
-  
-TxtRotate.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-  
-    if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-  
-    this.el.innerHTML = '<span className="wrap">'+this.txt+'</span>';
-  
-    var that = this;
-    var delta = 300 - Math.random() * 100;
-  
-    if (this.isDeleting) { delta /= 2; }
-  
-    if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
-      this.loopNum++;
-      delta = 500;
-    }
-  
-    setTimeout(function() {
-      that.tick();
-    }, delta);
-};
-  
-  window.onload = function() {
-    var elements = document.getElementsByClassName('txt-rotate');
-    for (var i=0; i<elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-rotate');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtRotate(elements[i], JSON.parse(toRotate), period);
-      }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-    document.body.appendChild(css);
 };
 
 const Typing = () => {
+    const [isActive, setIsActive] = useState(false);
+
+    useEffect(() => {
+        const handleVisibilityChange = () => {
+        setIsActive(document.visibilityState === 'visible');
+        };
+
+        document.addEventListener('visibilitychange', handleVisibilityChange);
+        
+        return () => {
+        document.removeEventListener('visibilitychange', handleVisibilityChange);
+        };
+    }, []);
+
+    useEffect(() => {
+        var elements = document.getElementsByClassName('txt-rotate');
+        for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-rotate');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            if (isActive) {
+            new TxtRotate(elements[i], JSON.parse(toRotate), period).tick();
+            }
+        }
+        }
+        // INJECT CSS
+        var css = document.createElement('style');
+        css.type = 'text/css';
+        css.innerHTML = '.txt-rotate > .wrap { border-right: 0.08em solid #666 }';
+        document.body.appendChild(css);
+    }, [isActive]);
+
     return (
-        <div className="typing">
-            <h1>We can <span
+        <div className={`typing ${isActive ? 'activa' : ''}`}>
+        <h1>
+            We can{' '}
+            <span
                 className="txt-rotate"
                 data-period="2000"
-                data-rotate='[ "create", "design", "develop" ]'>
-            </span>
-            <br/>
-            greats things together.</h1>
+                data-rotate='[ "create", "design", "develop" ]'
+            ></span>
+            <br />
+            great things together.
+        </h1>
         </div>
-    )
-}
+    );
+};
 
-export default Typing
+export default Typing;
