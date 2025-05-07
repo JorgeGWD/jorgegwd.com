48747da332ae1195a346d0f68b00759d8ca9c856
JorgeGWD <jorge.rodriguez.r@gmail.com>
2025-05-07 12:57:33 -0500

    Adjust and configure responsive navigation menu

--- src/Components/Common/Header/Navbar/Navbar.js
+++ src/Components/Common/Header/Navbar/Navbar.js
@@ -1,12 +1,30 @@
+import { useRef } from 'react'
 import './Navbar.css'
+import IconMenu from '../../../../assets/images/icon-menu.svg'
+import IconClose from '../../../../assets/images/icon-close.svg'
 
 const Navbar = () => {
+    const checkboxRef = useRef(null)
+
+    const closeMenu = () => {
+        if (checkboxRef.current) {
+            checkboxRef.current.checked = false;
+        }
+    }
+
     return (
         <nav>
-            <a href="#about">About</a>
-            <a href="#experience">Experience</a>
-            <a href="#portfolio">Portfolio</a>
-            <a href="#contact">Contact</a>
+            <input type="checkbox" id="check" ref={checkboxRef} />
+            <label htmlFor="check" className="navbar__content-checkbtn">
+                <img src={IconMenu} alt="Menu" className='open-menu' />
+                <img src={IconClose} alt="Close" className='close-menu' />
+            </label>
+            <div className="navbar__content-menu">
+                <a href="#about" onClick={closeMenu}>About</a>
+                <a href="#experience" onClick={closeMenu}>Experience</a>
+                <a href="#portfolio" onClick={closeMenu}>Portfolio</a>
+                <a href="#contact" onClick={closeMenu}>Contact</a>
+            </div>
         </nav>
     )
 }
