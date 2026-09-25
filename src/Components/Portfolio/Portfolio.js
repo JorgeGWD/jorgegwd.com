e686f6167e9ec0b5fcfadf45f11c973d4a6ce698
Jorge Rodriguez <jorge.rodriguez.r@gmail.com>
2026-09-25 14:53:49 -0500

    Remove unnecessary portfolio card focus

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './Portfolio.css'
 
 const Portfolio = () => {
@@ -98,27 +98,6 @@ const Portfolio = () => {
     },
   ])
 
-  useEffect(() => {
-    const items = document.querySelectorAll('.porfolio__data')
-
-    const handleFocus = (e) => {
-      e.target.scrollIntoView({
-        behavior: 'smooth',
-        block: 'center',
-      })
-    }
-
-    items.forEach((item) => {
-      item.addEventListener('focus', handleFocus)
-    })
-
-    return () => {
-      items.forEach((item) => {
-        item.removeEventListener('focus', handleFocus)
-      })
-    }
-  }, [])
-
   return (
     <div className="container" id="portfolio">
       <div className="portfolio content">
@@ -126,7 +105,7 @@ const Portfolio = () => {
           <h1>Portfolio</h1>
           <div className="portfolio__info big__card">
             {bigCard.map((data, key) => (
-              <div className="porfolio__data box" key={key} tabIndex="0">
+              <div className="porfolio__data box" key={key}>
                 <img src={data.image} alt={data.title} />
                 <div className="porfolio__data__text">
                   <h2>{data.title}</h2>
@@ -147,7 +126,7 @@ const Portfolio = () => {
           </div>
           <div className="portfolio__info small__card">
             {smallCard.map((data, key) => (
-              <div className="porfolio__data box" key={key} tabIndex="0">
+              <div className="porfolio__data box" key={key}>
                 <img src={data.image} alt={data.title} />
                 <div className="porfolio__data__text">
                   <h3>{data.title}</h3>
