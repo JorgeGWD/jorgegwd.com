c8e179c5567961c4443af4315ada0f619f8b2a0a
Jorge Rodriguez <jorge.rodriguez.r@gmail.com>
2026-09-25 15:31:08 -0500

    Add accessible names to social icon links

--- src/Components/Common/Header/Codes/Codes.js
+++ src/Components/Common/Header/Codes/Codes.js
@@ -4,8 +4,8 @@ import './Codes.css'
 const Codes = () => (
     <div className="codes">
         <ul>
-            <li><a href="https://codepen.io/JorgeGWD/" target="_blank" rel="noopener noreferrer"><i className="fab fa-codepen"></i></a></li>
-            <li><a href="https://github.com/JorgeGWD" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a></li>
+            <li><a href="https://codepen.io/JorgeGWD/" target="_blank" rel="noopener noreferrer" aria-label="CodePen"><i className="fab fa-codepen"></i></a></li>
+            <li><a href="https://github.com/JorgeGWD" target="_blank" rel="noopener noreferrer" aria-label="GitHub"><i className="fab fa-github"></i></a></li>
         </ul>
     </div>
 )
