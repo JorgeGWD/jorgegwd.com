9c56e825c3eaa09c3da1d9a72887f4d4b2044d13
Jorge Rodriguez <jorge.rodriguez.r@gmail.com>
2026-09-25 14:24:53 -0500

    Allow Button component to render accessible links

--- src/Components/Common/Button/Button.js
+++ src/Components/Common/Button/Button.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import './Button.css'
 
-const Button = ({ className, text, type, onClick }) => {
+const Button = ({ className, text, type, onClick, href, target, rel }) => {
+    if (href) {
+        return (
+            <a
+                className={className}
+                href={href}
+                target={target}
+                rel={rel}
+            >
+                <p>{text}</p>
+            </a>
+        )
+    }
+
     return (
         <button className={className} type={type} onClick={onClick}>
             <p>{text}</p>
