1a172a941ab85dbde1a5722f9fb84eaa1945d5d4
Jorge Rodriguez <jorge.rodriguez.r@gmail.com>
2026-09-25 15:21:21 -0500

    Update footer professional title

--- src/Components/Common/Footer/Footer.js
+++ src/Components/Common/Footer/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <footer>
             <div className="footer__content">
-                <p>JorgeGWD | Jorge Graphic & Web Designer</p>
+                <p>JorgeGWD | Frontend Developer</p>
                 {/* <p>{new Date().getFullYear()}</p> */}
             </div> 
         </footer>
